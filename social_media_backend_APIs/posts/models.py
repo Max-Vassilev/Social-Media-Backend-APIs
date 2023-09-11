f7e0d281eb3b71c12db0967392f4aee7283471d2
Max-Vassilev <maximvassilev01@gmail.com>
2023-09-11 23:03:24 +0300

    Created the "Post" model

--- social_media_backend_APIs/posts/models.py
+++ social_media_backend_APIs/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+    created_at = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        if len(self.content) < 20:
+            return f"Post: {self.content}"
+        else:
+            return f"Post: {self.content[0: 15]}..."
