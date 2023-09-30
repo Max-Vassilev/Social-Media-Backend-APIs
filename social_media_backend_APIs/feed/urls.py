6b0a3407b37daa637d31ed6301b794932cd8529e
Max-Vassilev <maximvassilev01@gmail.com>
2023-09-30 14:54:49 +0300

    Created "create-post" API

--- social_media_backend_APIs/feed/urls.py
+++ social_media_backend_APIs/feed/urls.py
@@ -4,7 +4,7 @@ from social_media_backend_APIs.feed.views import *
 urlpatterns = [
 
     path("all-posts/", AllPostsViewAPI.as_view(), name="all posts"),
-    # path("create-post/", CreatePostViewAPI.as_view(), name="create post"),
+    path("create-post/", CreatePostViewAPI.as_view(), name="create post"),
     # path("delete-post/<int:pk>/", DeletePostViewAPI.as_view(), name="soft delete post"),
     #
     # path("like-post/<int:pk>/", LikePostViewAPI.as_view(), name="like post"),
--- social_media_backend_APIs/feed/views.py
+++ social_media_backend_APIs/feed/views.py
@@ -12,8 +12,24 @@ class ViewPostSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CreatePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["content"]
+
+
 class AllPostsViewAPI(api_views.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = ViewPostSerializer
     # Work if user is authenticated:
     permission_classes = [IsAuthenticated]
+
+
+class CreatePostViewAPI(api_views.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = CreatePostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically set the author as the currently authenticated user
+        serializer.save(author=self.request.user)
