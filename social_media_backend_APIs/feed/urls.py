384c309fc524ef2a80e1888c70154f1c1f273b00
Max-Vassilev <maximvassilev01@gmail.com>
2023-10-01 14:47:18 +0300

    Worked on the landing page template

--- social_media_backend_APIs/feed/urls.py
+++ social_media_backend_APIs/feed/urls.py
@@ -3,9 +3,9 @@ from social_media_backend_APIs.feed.views import *
 
 urlpatterns = [
 
-    path("all-posts/", AllPostsViewAPI.as_view(), name="all posts"),
-    path("create-post/", CreatePostViewAPI.as_view(), name="create post"),
-    path("delete-post/<int:pk>/", DeletePostViewAPI.as_view(), name="delete post"),
+    path("all-posts/", AllPostsViewAPI.as_view(), name="all posts API"),
+    path("create-post/", CreatePostViewAPI.as_view(), name="create post API"),
+    path("delete-post/<int:pk>/", DeletePostViewAPI.as_view(), name="delete post API"),
     #
     # path("like-post/<int:pk>/", LikePostViewAPI.as_view(), name="like post"),
     # path("unlike-post/<int:pk>/", UnlikePostViewAPI.as_view(), name="unlike post"),
